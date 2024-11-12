f015b066065d4472710a88481ae3d424f3e8dbd4
MortenJ31 <mortenjeppesen91@gmail.com>
2024-11-12 09:27:58 +0100

    Oprettelse af Model mappe og classer

---
+++ APIServer/Model/Ad.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace APIServer.Model
+{
+	public class Ad
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string UserId { get; set; } // Reference til User
+
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public double Price { get; set; }
+		public string Status { get; set; }
+		public string ImageUrl { get; set; }
+		public DateTime CreatedAt { get; set; }
+
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string CategoryId { get; set; } // Reference til Category
+
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string LocationId { get; set; } // Reference til Location
+	}
+}
---
+++ APIServer/Model/Location.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace APIServer.Model
+{
+	public class Location
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+
+		public string Name { get; set; }
+		public string Classroom { get; set; } // Nyt felt i modellen
+	}
+}
+
