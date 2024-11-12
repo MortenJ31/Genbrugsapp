b79c3f70c5b6844aa550160bceb72905211ddba6
MortenJ31 <mortenjeppesen91@gmail.com>
2024-11-12 09:40:56 +0100

    Lille opdatering

---
+++ APIServer/Services/MongoDbService.cs
@@ -0,0 +1,19 @@
+﻿using APIServer.Model;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace APIServer.Services
+{
+	public class MongoDbService
+	{
+		private readonly IMongoDatabase _database;
+
+		public MongoDbService(IOptions<MongoDbSettings> mongoDbSettings, IMongoClient mongoClient)
+		{
+			_database = mongoClient.GetDatabase(mongoDbSettings.Value.DatabaseName);
+		}
+
+		// Eksempel på at få en User Collection
+		public IMongoCollection<User> Users => _database.GetCollection<User>("User");
+	}
+}
